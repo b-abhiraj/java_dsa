9dc737dc9c43449d4f42603876886e282798bd62
abhiraj <budnurabhiraj@outlook.com>
2025-07-21 10:01:14 +0530

    day4 package

--- HackerRank_DSA/CamelCase.java
+++ HackerRank_DSA/CamelCase.java
@@ -1,11 +1,15 @@
 package HackerRank_DSA;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.lang.Character;
 
 public class CamelCase {
 
-	public static void main(String[] args) {
-		String s = "saveChangesInTheEditor";
+	public static void main(String[] args) throws IOException {
+		BufferedReader br= new BufferedReader(new InputStreamReader(System.in));
+		String s = br.readLine();
 		int count = 1;
 		for (int i = 0; i < s.length(); i++) {
 			if (Character.isUpperCase(s.charAt(i))) {
